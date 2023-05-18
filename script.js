431c28ac302749b242abd90b904b013490035044
fredabila <55893301+fredabila@users.noreply.github.com>
2023-05-18 12:44:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// Add your custom JavaScript code here (if needed)
+// Example: Scroll to section on click
+document.addEventListener('DOMContentLoaded', function() {
+  var ctaButton = document.querySelector('.app-store-link');
+  var featuresSection = document.querySelector('#features');
+
+  ctaButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    featuresSection.scrollIntoView({ behavior: 'smooth' });
+  });
+});
